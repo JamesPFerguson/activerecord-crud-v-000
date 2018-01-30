d5840566007a4eeb7594e4fa4d7ab4778e8ad7ee
James Ferguson <ncjamesferguson@gmail.com>
2018-01-30 22:52:02 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -11,14 +11,7 @@ def can_be_instantiated_and_then_saved
   movie
 end
 
-def can_be_created_with_a_hash_of_attributes
-  attributes = {
-      title: "The Sting",
-      release_date: 1973,
-      director: "George Roy Hill",
-      lead: "Paul Newman",
-      in_theaters: false
-  }
+def can_be_created_with_a_hash_of_attributes(attributes)
   movie = Movie.new
   attributes.each {|key, value| movie.send("#{key}=", value)}
 end
