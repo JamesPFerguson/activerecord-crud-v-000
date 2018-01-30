d2ae1984c53c7dcccc99b3bfefb4ec9046987aa5
James Ferguson <ncjamesferguson@gmail.com>
2018-01-30 22:53:47 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -11,8 +11,16 @@ def can_be_instantiated_and_then_saved
   movie.save
 end
 
-def can_be_created_with_a_hash_of_attributes(attributes)
-  movie = Movie.create(attributes)
+def can_be_created_with_a_hash_of_attributes
+  attributes = {
+      title: "The Sting",
+      release_date: 1973,
+      director: "George Roy Hill",
+      lead: "Paul Newman",
+      in_theaters: false
+  }
+  movie = Movie.new
+  attributes.each {|key, value| movie.send("#{key}=", value)}
 end
 
 def can_be_created_in_a_block
